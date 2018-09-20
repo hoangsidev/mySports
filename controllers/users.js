d5d9e9a1c53c771922b67044c7a7e60423d970cf
Si Hoang <>
2018-09-20 17:59:41 +0700

    Update limit field

--- controllers/users.js
+++ controllers/users.js
@@ -4,31 +4,27 @@ const insertToken = require('../helpers/auth.js').insertToken;
 const md5 = require('md5');
 const toJson = require('../helpers/functions.js').toJson;
 const resData = require('../helpers/functions.js').resData;
+const limitField = require('../helpers/functions.js').limitField;
 const objectId = require('mongodb').ObjectID;
 
 module.exports = {
     signUp: (req, res) => {
-        var data = req.payload;
-        data.password = md5(data.password);
         return new Promise((resolve, reject) => {
+            var data = req.payload;
+            data.password = md5(data.password);
+            const select = ['phoneNumber', 'fullName'];
             users.findOne({
-                condition: { phoneNumber: data.phoneNumber }, select: ['phoneNumber']
+                condition: { phoneNumber: data.phoneNumber }, select: select
             }).then(existedUser => {
                 if (existedUser) {
-                    resolve(resData(201, false, 'Existed Phone Number!', {}));
+                    resolve(resData(201, false, 'Existed phone number!', {}));
                 } else {
-                    users.insert(data).then(newUser => {
-                        if (newUser) {
-                            users.findOne({
-                                condition: { _id: objectId(newUser._id) }, select: ['fullName']
-                            }).then(userInfo => {
-                                const token = createToken(userInfo._id);
-                                insertToken(token);
-                                resolve(resData(201, true, null, {
-                                    token: token,
-                                    user: userInfo
-                                }));
-                            });
+                    users.insert(data).then(user => {
+                        if (user) {
+                            user = toJson(limitField(select, user));
+                            const token = createToken(user._id);
+                            insertToken(token);
+                            resolve(resData(201, true, null, { token, user }));
                         }
                     }).catch(error => { reject(error); });
                 }
@@ -36,15 +32,38 @@ module.exports = {
         });
     },
 
-    getUserByUserId: (req, res) => {
-        var data = req.params;
+    signIn: (req, res) => {
+        var data = req.payload;
+        data.password = md5(data.password);
         return new Promise((resolve, reject) => {
+            const select = ['fullName'];
+            users.findOne({
+                condition: { phoneNumber: data.phoneNumber }, select: select
+            }).then(user => {
+                if (!user) {
+                    resolve(resData(201, false, 'Phone number not exist!', {}));
+                } else {
+                    user = toJson(user);
+                    const token = createToken(user._id);
+                    insertToken(token);
+                    resolve(resData(201, true, null, { token, user }));
+                }
+            }).catch(error => { reject(error); });
+        });
+    },
+
+    getUserByUserId: (req, res) => {
+        return new Promise((resolve, reject) => {
+            var data = req.params;
             users.findOne({
                 condition: { _id: objectId(data.userId) }, select: ['fullName']
-            }).then(result => {
-                resolve(resData(201, true, null, {
-                    user: result
-                }));
+            }).then(user => {
+                if (user) {
+                    user = toJson(user);
+                    resolve(resData(201, true, null, { user }));
+                } else {
+                    resolve(resData(201, false, 'User not found!', {}));
+                }
             });
         });
     }
--- helpers/functions.js
+++ helpers/functions.js
@@ -1,7 +1,7 @@
 const objectId = require('mongodb').ObjectID;
 module.exports = {
     toJson: (data) => {
-        return JSON.parse(JSON.stringify(data));;
+        return JSON.parse(JSON.stringify(data));
     },
     resData: (statusCode, success, message, data) => {
         return {
@@ -10,5 +10,13 @@ module.exports = {
             message: message,
             data: data
         };
+    },
+    limitField: (arrSelect, object) => {
+        var newObject = {};
+        object = JSON.parse(JSON.stringify(object));
+        arrSelect.map(key => {
+            newObject[key] = object[key];
+        });
+        return newObject;
     }
 }
